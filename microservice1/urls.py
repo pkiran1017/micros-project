8bec426712a62c2a10c38a174bcde652dc5d2e9c
pkiran1017 <p1432k@gmail.com>
2023-10-12 18:09:57 +0530

    first comm

--- microservice1/urls.py
+++ microservice1/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('endpoint1/', views.endpoint1, name='endpoint1'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
    
 ]
